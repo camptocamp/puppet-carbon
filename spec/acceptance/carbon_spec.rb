827b27abe4ce9d078598b09137e53c399840c858
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2015-08-19 10:54:18 +0200

    added some more tests in order to ensure we actually use ports

--- spec/acceptance/carbon_spec.rb
+++ spec/acceptance/carbon_spec.rb
@@ -2,13 +2,54 @@ require 'spec_helper_acceptance'
 
 describe 'carbon' do
   context 'with defaults' do
-    it 'should idempotently run' do
+    it 'is_expected.to idempotently run' do
       pp = <<-EOS
         class { 'carbon': }
       EOS
 
       apply_manifest(pp, :catch_failures => true)
       apply_manifest(pp, :catch_changes => true)
+    end
+
+    describe service('carbon-relay') do
+      it { is_expected.to be_enabled }
+      it { is_expected.to be_running }
+    end
+    describe port(2003) do
+      it { is_expected.not_to be_listening }
+    end
+    describe port(2004) do
+      it { is_expected.not_to be_listening }
+    end
+
+    describe port(2103) do
+      it { is_expected.to be_listening }
+    end
+    describe port(2104) do
+      it { is_expected.to be_listening }
+    end
+    describe port(7102) do
+      it { is_expected.to be_listening }
+    end
+
+    describe port(2113) do
+      it { is_expected.to be_listening }
+    end
+    describe port(2114) do
+      it { is_expected.to be_listening }
+    end
+    describe port(7112) do
+      it { is_expected.to be_listening }
+    end
+
+    describe port(2123) do
+      it { is_expected.to be_listening }
+    end
+    describe port(2124) do
+      it { is_expected.to be_listening }
+    end
+    describe port(7122) do
+      it { is_expected.to be_listening }
     end
   end
 end
