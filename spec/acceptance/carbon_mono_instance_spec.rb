522b5fb37ff1937bfdaf61d21b60463405949841
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2015-08-19 10:54:33 +0200

    new acceptance spec file for mono_instance

---
+++ spec/acceptance/carbon_mono_instance_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper_acceptance'
+
+describe 'carbon class' do
+  context 'carbon mono_instance' do
+    it 'is_expected.to idempotently run' do
+      pp = <<-EOS
+        class { 'carbon': 
+          mono_instance => true,
+        }
+      EOS
+
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes => true)
+    end
+
+    describe service('carbon-cache') do
+      it { is_expected.to be_enabled }
+      it { is_expected.to be_running }
+    end
+    describe port(2003) do
+      it { is_expected.to be_listening }
+    end
+    describe port(2004) do
+      it { is_expected.to be_listening }
+    end
+  end
+end
